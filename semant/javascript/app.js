7957caa0f4b0c22a25be43da4a58e7bfee8c3f19
BaharKrc <144059055+BaharKrc@users.noreply.github.com>
2024-01-16 15:11:34 +0100

    js

---
+++ semant/javascript/app.js
@@ -0,0 +1,32 @@
+
+console.log("APP JS RUNNING")
+
+const pi = 3.14
+
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3"
+console.log(typeof sayi1);
+
+const harDu = true
+const isOpen = false
+console.log(isOpen, typeof isOpen);
+
+let language = "Java"
+console.log(language, typeof language);
+
+language = "Javascript"
+console.log(language);
+
+language = true 
+console.log(language);
+
+language = 3.14
+console.log(language);
+
+let adet
+console.log(adet);
+
+adet = "23"
+console.log(typeof adet);
